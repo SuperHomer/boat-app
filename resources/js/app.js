fc947bd42c06906401ec2b2de075f5737ed219de
Yoan Marti <yoan.marti@gmail.com>
2024-01-05 13:21:23 +0100

    Add vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import Boats from './components/Boats.vue';
+
+const app = createApp({
+    components: {
+        Boats,
+    }
+});
+
+app.mount('#app');
---
+++ resources/js/components/Boats.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>Boats</h1>
+  </div>
+</template>
